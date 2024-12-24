ddacf5cc4a0287086810c816cb639efc69c46671
md.munna <mk8761174@gmail.com>
2024-12-24 13:58:18 +0600

    add search option in all service page

--- src/components/BookNow.jsx
+++ src/components/BookNow.jsx
@@ -166,7 +166,7 @@ const BookNow = () => {
                         </div>
 
                         <div>
-                            <label className="text-gray-700" htmlFor="serviceTakingDate">Service Taking Date</label>
+                            <label className="text-gray-700"  htmlFor="serviceTakingDate">Service Taking Date</label>
                             <DatePicker
                                 selected={startDate}
                                 onChange={(date) => setStartDate(date)}
@@ -177,11 +177,12 @@ const BookNow = () => {
                         </div>
 
                         <div className="col-span-2">
-                            <label className="text-gray-700" htmlFor="comment">Special Instruction</label>
+                            <label className="text-gray-700" required htmlFor="comment">Special Instruction</label>
                             <textarea
                                 id="comment"
                                 name="comment"
                                 rows="4"
+                                required
                                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
                             ></textarea>
                         </div>
--- src/pages/AllServices.jsx
+++ src/pages/AllServices.jsx
@@ -1,84 +1,18 @@
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
 
-// const AllServices = () => {
-//   const [services, setServices] = useState([]);
-
-//   useEffect(() => {
-//     fetchAllServices();
-//   }, []);
-
-//   const fetchAllServices = async () => {
-//     try {
-//       const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/services`);
-//       setServices(data);
-//     } catch (error) {
-//       console.error("Error fetching services:", error);
-//     }
-//   };
-
-//   return (
-//     <div className="container mx-auto my-12">
-//       <h1 className="text-2xl font-bold mb-8">All Services ({services.length})</h1>
-//       <div className="grid grid-cols-1 gap-6">
-//         {services.map((service) => (
-//           <div key={service.id} className="p-6 bg-white rounded-lg grid grid-cols-6 shadow-md">
-//           <div className="col-span-3">
-//           <img src={service.image} alt={service.title} className="w-full h-[400px] object-cover rounded-md" />
-//           </div>
-//           <div className="col-span-2">
-//           <div>
-//            <h2 className="text-xl font-semibold mt-4">{service.title}</h2>
-//             <p className="text-gray-700 mt-2">
-//               {service.description.length > 100
-//                 ? `${service.description.substring(0, 100)}...`
-//                 : service.description}
-//             </p>
-//            </div>
-//             <div className="flex items-center mt-4">
-//               <img src={service.buyer.photo} alt={service.buyer.name} className="w-10 h-10 object-cover rounded-full" />
-//               <div className="ml-3">
-//                 <p className="text-gray-700 font-medium">{service.buyer.name}</p>
-//               </div>
-//             </div>
-//         <div>
-//               <p className="text-gray-700 mt-2">Area: {service.area}</p>
-//             <p className="text-gray-700 mt-2">Price: ${service.service_price.toFixed(2)}</p>
-//         </div>
-//          <div>
-//          <Link to={`/service-details/${service._id}`}>
-           
-//            <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-//               View Detail
-//             </button>
-//            </Link>
-//          </div>
-//           </div>
-
-
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AllServices;
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const AllServices = () => {
   const [services, setServices] = useState([]);
-
+  const [search, setSearch] = useState('');
   useEffect(() => {
     fetchAllServices();
-  }, []);
+  }, [search]);
 
   const fetchAllServices = async () => {
     try {
-      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/services`);
+      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/services?search=${search}`);
       setServices(data);
     } catch (error) {
       console.error("Error fetching services:", error);
@@ -87,6 +21,19 @@ const AllServices = () => {
 
   return (
     <div className="container mx-auto my-12 px-4">
+    {/* Search Bar */}
+    <div className="w-[400px] mx-auto mb-6">
+               
+                <input
+                    onChange={(e) => setSearch(e.target.value)}
+                    type="text"
+                    name="search"
+                    placeholder="Search for movies..."
+                    className="input input-bordered w-full p-4 rounded-xl shadow-md transition-all hover:shadow-xl focus:outline-none"
+                    required
+                />
+            </div>
+
       <h1 className="text-4xl font-bold text-center mb-12 text-gray-800">All Services ({services.length})</h1>
       <div className="flex flex-col gap-8">
         {services.map((service) => (
